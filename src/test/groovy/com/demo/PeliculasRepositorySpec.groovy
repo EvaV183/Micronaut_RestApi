2844808b4450a94d1d1a9fd2f5e86e2be2bd09d2
Eva Villa <eva.villa183@gmail.com>
2025-05-20 13:06:23 +0200

    Add PeliculasRepository Test

---
+++ src/test/groovy/com/demo/PeliculasRepositorySpec.groovy
@@ -0,0 +1,36 @@
+package com.demo
+
+import com.application.Fixtures
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Specification
+
+@MicronautTest
+class PeliculasRepositorySpec extends Specification {
+    @Inject
+    @Client("/")
+    HttpClient client
+
+    @Inject
+    Fixtures fixtures
+
+    void setup() {
+        fixtures.cleanDatabase()
+    }
+
+    @Inject
+    PeliculasRepository peliculasRepository
+
+    void "Test que guarda la pelicula usando el metodo save"() {
+        when:
+        def pelicula = peliculasRepository.save("Oppenheimer", "Christopher Nolan")
+
+        then:
+        pelicula != null
+        pelicula.id != null
+        pelicula.titulo == "Oppenheimer"
+        pelicula.director == "Christopher Nolan"
+    }
+}
